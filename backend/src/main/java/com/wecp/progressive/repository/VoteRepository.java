1d08c38fa55d9fd8864d3f09e90a00719e48fe44
username <wecp-dummy@gmail.com>
2025-01-07 15:21:24 +0000

    day10

--- backend/src/main/java/com/wecp/progressive/repository/VoteRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/VoteRepository.java
@@ -1,5 +1,22 @@
 package com.wecp.progressive.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface VoteRepository {
+import com.wecp.progressive.entity.Vote;
+
+@Repository
+public interface VoteRepository extends JpaRepository<Vote , Integer>{
+    
+    @Query("SELECT COUNT(v) FROM Vote v WHERE v.category = :category")
+    Long countByCategory(String category);
+
+    @Query("DELETE FROM Vote t WHERE t.team.teamId = :teamId")
+    void deleteByTeamId(int teamId);
+
+    @Query("DELETE FROM Vote c WHERE c.cricketer.cricketerId = :cricketerId")
+    void deleteByCricketerId(int cricketerId);
+
 }
--- backend/src/main/java/com/wecp/progressive/service/impl/CricketerServiceImplJpa.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/CricketerServiceImplJpa.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Service
 public class CricketerServiceImplJpa implements CricketerService {
 
+    @Autowired
     private CricketerRepository cricketerRepository;
 
     @Autowired
